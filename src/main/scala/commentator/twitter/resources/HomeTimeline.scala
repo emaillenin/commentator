0763de95631c135a23d29dfaae09a9c7fd305bf3
Lenin <lenin.rajasekaran@bigcommerce.com>
2017-07-27 22:18:24 +1000

    Refactor HomeTimeline - Make constants reusable

--- src/main/scala/commentator/twitter/resources/HomeTimeline.scala
+++ src/main/scala/commentator/twitter/resources/HomeTimeline.scala
@@ -10,25 +10,27 @@ import scala.collection.JavaConversions._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
-class HomeTimeline(implicit system: ActorSystem) extends Actor with TwitterInstance with RedisInstance with LazyLogging {
+object HomeTimeline {
   val maxPages = 10
   val maxTrendingTags = 10
+}
+class HomeTimeline(implicit system: ActorSystem) extends Actor with TwitterInstance with RedisInstance with LazyLogging {
 
   override def receive = {
     case TrackTrendingTags(commentatorName: String) =>
       val trendingTagsKey = s"commentator/$commentatorName/twitter/trending_tags"
 
       redis.lpush(trendingTagsKey, trendingTags: _*)
-      redis.ltrim(trendingTagsKey, 0, maxTrendingTags - 1)
+      redis.ltrim(trendingTagsKey, 0, HomeTimeline.maxTrendingTags - 1)
       system.scheduler.scheduleOnce(2 hours, self, TrackTrendingTags(commentatorName))
   }
 
   private def trendingTags = {
-    tagsFromTimeline.groupBy(identity).toList.sortBy(-1 * _._2.size).map(_._1).take(maxTrendingTags)
+    tagsFromTimeline.groupBy(identity).toList.sortBy(-1 * _._2.size).map(_._1).take(HomeTimeline.maxTrendingTags)
   }
 
   private def tagsFromTimeline = {
-    Range(1, maxPages).flatMap(page => {
+    Range(1, HomeTimeline.maxPages).flatMap(page => {
       Thread.sleep(20000)
       tagsFromPage(page)
     })
